f7c110842f9528661d573bc08ce2cb21f365f710
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-21 07:52:31 +0000

    Implemented Fighter, Rouge, Magician, and Cleric classes

--- character.h
+++ character.h
@@ -12,10 +12,36 @@ class Character {
 protected:
     std::string name;
     double HP, MP;
-    int strength, Dexterity, Intelligence, Speed, Endurance, Faith;
+    int Strength, Dexterity, Intelligence, Speed, Endurance, Faith;
 
 public:
     Character(std::string n) : name(n) {}
     virtual ~Character() {}
     virtual std::string toString() const = 0;
 };
+
+class Fighter : public Character {
+public:
+    Fighter(std::string n);
+    std::string toString() const override;
+};
+
+class Rogue : public Character {
+public:
+    Rogue(std::string n);
+    std::string toString() const override;
+};
+
+class Magician : public Character {
+public:
+    Magician(std::string n);
+    std::string toString() const override;
+};
+
+class Cleric : public Character {
+public:
+    Cleric(std::string n);
+    std::string toString() const override;
+};
+
+#endif
